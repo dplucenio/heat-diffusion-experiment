4359912ce0898c6c4a8d54c7328fb11a7eb486c9
Daniel Plucenio <dplucenio@gmail.com>
2016-10-07 17:39:59 -0300

    Enable module to be compiled with msvc and gcc compilers

--- setup.py
+++ setup.py
@@ -1,13 +1,23 @@
 from setuptools import setup, Extension
 import numpy
+import sys
+
+if sys.platform == 'linux'
+    extra_compile_args = ['-fopenmp'
+    extra_link_args = ['-fopenmp']
+else:
+    extra_compile_args = ['-/openmp']
+    extra_link_args = ['-/openmp']
+
+
 
 ext_modules=[
     Extension(
         "heat_diffusion_experiment.cython_versions",
         ["heat_diffusion_experiment/cython_versions.pyx"], 
         language='c++',
-        extra_compile_args=['/openmp'],
-#         extra_link_args=['/openmp'],
+        extra_compile_args=extra_compile_args,
+        extra_link_args=extra_link_args,
     ),
 ]
 
